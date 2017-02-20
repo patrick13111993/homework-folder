18c0f542116d172895dfe34fc9a7912efbb76903
patrick13111993 <keriakospapadoopalus@gmail.com>
2017-02-20 15:28:18 +0000

    rails

---
+++ week15/monday/shows_api/db/seeds.rb
@@ -0,0 +1,17 @@
+Show.delete_all()
+
+Show.create({
+  title: "Spongebob Squarepants",
+  series: 3,
+  description: "Spongebob pisses Squidward off again.",
+  image: "http://cdn4.thr.com/sites/default/files/2013/11/spongebob_a.jpg",
+  programmeID: "cgkj4t34" 
+})
+
+Show.create({
+  title: "Rick and Morty",
+  series: 1,
+  description: "Space adventures.",
+  image: "http://screencrush.com/442/files/2017/01/rick-and-morty-season-3-pic.jpg?w=720&cdnnode=1",
+  programmeID: "ft39tn35" 
+})
